010a8ec708c73eda72dbdc0ef9c997876d82899e
ThundaHorse <abeeeeekim@gmail.com>
2019-07-31 15:25:09 -0400

    added simple click counter

--- src/index.js
+++ src/index.js
@@ -3,9 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root')
+);
 
+let model = { clicks: 0 };
+function render() {
+  ReactDOM.render(<App
+     clicks={model.clicks} 
+     onClick={() => { model.clicks += 1; render(); }}/>, document.getElementById('root'));
+
+}
+
+render(); 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
